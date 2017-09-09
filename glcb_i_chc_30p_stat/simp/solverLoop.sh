8ee6774e427bd0ec11e9bf6e57d9ba4c1aeb947e
ljl7 <ljl7@sfu.ca>
2017-09-08 17:41:41 -0700

    adding clb_i_chc_30p_stat

---
+++ glcb_i_chc_30p_stat/simp/solverLoop.sh
@@ -0,0 +1,39 @@
+make
+
+for i in *.cnf; do
+	if [ -s "$i.cmty" ]; then
+	   if [ -f "$i.clauseCommBrg.graphxcl.betwCentScoresInvSquareNormalized" ]; then
+			if [ -f "$i.centrality-n-50" ]; then
+				if [ ! -f "$i.txt_i_hc_30p" ]; then
+					echo $i		
+					start=`date +%s`
+					timeout 5000 ./glcb_i_hc_30p "-cmty-file=$i.cmty" "-cnf-file=$i"  "-center-file=$i.clauseCommBrg.graphxcl.betwCentScoresInvSquareNormalized" $i > "$i.txt_i_hc_30p"
+					end=`date +%s`
+					runtime=$((end-start))
+					echo runtime_seconds glcb_i_hc_30p : $runtime
+				fi				
+			fi
+	    fi
+	fi
+done
+
+
+for i in *.cnf; do
+	if [ -s "$i.cmty" ]; then
+	   if [ -f "$i.clauseCommBrg.graphxcl.betwCentScoresInvSquareNormalized" ]; then
+			if [ -f "$i.centrality-n-50" ]; then
+				if [ ! -f "$i.txt_i_hc_30p" ]; then
+					echo $i		
+					start=`date +%s`
+					timeout 5000 ./glcb_i_hc_30p "-cmty-file=$i.cmty" "-cnf-file=$i"  "-center-file=$i.clauseCommBrg.graphxcl.betwCentScoresInvSquareNormalized" $i > "$i.txt_i_hc_30p"
+					end=`date +%s`
+					runtime=$((end-start))
+					echo runtime_seconds glcb_i_hc_30p : $runtime
+				fi				
+			fi
+	    fi
+	fi
+done
+
+
+#if [ ! -s "$i.txt_a_hc_cutoff" ]; then
